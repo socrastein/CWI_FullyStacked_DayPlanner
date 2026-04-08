865e30632a3acad7636022f1bf3de2a3a2092d3c
Caleb Trisler <Calebtrisler@gmail.com>
2026-04-08 08:35:25 -0600

    Removed validation and added testing for holiday Event
    added testing for holidayEvent
    
    Deleted validator functions validateYear, validateMonth,validateWeekDay and their calls to remove redundant protection now that the class functions as intended.
    
    Kept validation for year public function export.
    
    Created a new testing file for holidayEvents
    
    Manually added the correct list of titles and dates.
    
    Spent an hour and a half doing the tinkering before checking out how others formatted their tests.
    
    Created a function to create a pass/fail test.
    
    Added a test to make sure the number of holidays matched expected.
    
    Looped to check sure that every expected holiday returned a match
    
    Tested that each holidayevent matched the verified correct date.
    
    Added a check to see if unexpected holiday’s get returned.
    
    Straight up ripped off the console.log outputs to keep them uniform and because I like the emoticons.
    Added exported function to end of runTests function call list.

--- tests/runTests.js
+++ tests/runTests.js
@@ -2,12 +2,14 @@ import calendarNavigationFunctionsTest from "./calendar-navigation-test";
 import eventTests from "./classCalendarEvent_test";
 import storageTests from "./dataStorage_test";
 import settingsTests from "./settings_test";
+import holidayTests from "./holidayTesting";
 
 function runTests() {
   eventTests();
   storageTests();
   settingsTests();
   calendarNavigationFunctionsTest();
+  holidayTests();
 }
 
 export default runTests;
